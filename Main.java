ce4c731dd726e086efdc186ec8377345248e5fe0
matteopallagrosi <57724762+matteopallagrosi@users.noreply.github.com>
2023-03-25 16:54:48 +0100

    Update Main.java
    deleted row

--- Main.java
+++ Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String args[]) {
         int i = 0;
         System.out.println("Hello World");
-        System.out.println("This is a test");
     }
 }
